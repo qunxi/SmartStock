26046eb7086eca5510613da09207c2473e61cc53
qunxi <wangqunxi@gmail.com>
2015-03-17 17:58:29 +0800

    add bootstrapper

---
+++ SamrtStock_Desktop/SmartStockBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Practices.Prism.UnityExtensions;
+
+namespace SamrtStock_Desktop
+{
+    public class SmartStockBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
